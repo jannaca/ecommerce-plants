c8542b682d3e4fc15f2888966808350a249808c9
Jannaca <janna.carolina19@gmail.com>
2023-11-03 21:07:36 -0400

    tests accounts app

--- accounts/tests.py
+++ accounts/tests.py
@@ -3,22 +3,55 @@ from django.urls import reverse
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import AuthenticationForm
 
+class TestSignupView(TestCase):
+
+    def setUp(self) -> None:
+        self.valid_data = {
+            'username': 'testuser',
+            'first_name': 'John',
+            'last_name': 'Doe',
+            'email': 'testuser@example.com',
+            'address': '123 Main St',
+            'password1': 'testpassword123',
+            'password2': 'testpassword123'
+
+        }
+        self.bad_data = {
+            'username': 'testuser',
+            'first_name': 'John',
+            'last_name': 'Doe',
+            'email': 'testuser@example.com',
+            'address': '123 Main St',
+            'password1': 'testpassword123',
+            'password2': 'otherpassword',
+        }
+    def test_signup_view_sucessful(self):
+        response = self.client.post(reverse('signup'), self.valid_data)
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse('index'))
+        self.assertTrue(User.objects.filter(username='testuser').exists())
+        
+        
+
+    def test_signup_invalid(self):
+        msg = "<li>Los dos campos de contrase"
+        response = self.client.post(reverse('signup'), self.bad_data)
+        self.assertEqual(response.status_code, 200)
+        self.assertFalse(User.objects.filter(username='testuser').exists())
+        self.assertTrue(msg in str(response.content))
+        # print("*" * 28)
+        # print(response.content)
+        
 class TestLoginView(TestCase):
     def test_login_view_get(self):
-        # Realiza una solicitud GET a la vista de inicio de sesión
+
         response = self.client.get(reverse('login'))
-
-        # Verifica que la página de inicio de sesión devuelva un código de estado 200 (OK)
         self.assertEqual(response.status_code, 200)
-
-        # Verifica que el formulario de inicio de sesión se muestre en la respuesta
         self.assertIsInstance(response.context['form'], AuthenticationForm)
 
     def test_login_view_post(self):
-        # Crea un usuario de prueba para usar en la prueba de inicio de sesión
         User.objects.create_user(username='testuser', password='testpassword')
 
-        # Datos de prueba para el inicio de sesión
         login_data = {
             'username': 'testuser',
             'password': 'testpassword',
@@ -26,9 +59,5 @@ class TestLoginView(TestCase):
 
         # Realiza una solicitud POST con los datos de inicio de sesión
         response = self.client.post(reverse('login'), data=login_data, follow=True)
-
-        # Verifica que la página redirija al usuario después del inicio de sesión
         self.assertRedirects(response, reverse('index'))
-
-        # Verifica que el usuario esté autenticado después del inicio de sesión
         self.assertTrue(response.context['user'].is_authenticated)
